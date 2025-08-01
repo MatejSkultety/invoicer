359b6f6769d956913e64c010c98cf763eca6ea64
Matej Skultety <matej.skultety.official@gmail.com>
2025-08-01 18:54:17 +0200

    refactor: restructure frontend folder for scalability

---
+++ frontend/src/components/HelloWorld.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <h3>Hello World Component</h3>
+  </div>
+</template>
+
+<script setup>
+// HelloWorld component logic
+</script>
---
+++ frontend/src/views/HomeView.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <h2>Home View</h2>
+    <p>Welcome to the Invoicer Home Page!</p>
+  </div>
+</template>
+
+<script setup>
+// Home view logic
+</script>
