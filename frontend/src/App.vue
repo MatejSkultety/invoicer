835f5e4e28d10937f4087ff278fb048e0906f961
Matej Skultety <matej.skultety.official@gmail.com>
2025-08-01 18:12:17 +0200

    feat: implement basic frontend folder

---
+++ frontend/src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1>Welcome to Invoicer Frontend (Vue.js)!</h1>
+  </div>
+</template>
+
+<script setup>
+// Vue 3 script setup
+</script>
+
+<style>
+h1 {
+  color: #42b983;
+}
+</style>
